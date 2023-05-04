536155dc09e902a23f0afced8f97f9c203391617
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 22:33:22 +0530

    update

--- src/student/queries.js
+++ src/student/queries.js
@@ -1,7 +1,9 @@
 const getStudents = "SELECT * FROM students";
 const getStudentsById = "SELECT * FROM students WHERE id = $1"; //$1 is a parameter not a constant
+const checkEmailExists = "SELECT s FROM students s WHERE s.email = $1";
 
 module.exports = {
   getStudents,
   getStudentsById,
+  checkEmailExists,
 };
