f6f150d98953bd8f8590d7cdfaa202e3c61537c4
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 22:18:47 +0530

    update

--- src/student/routes.js
+++ src/student/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const controller = require('./controller');
+const controller = require("./controller");
 
 const router = Router();
 
 router.get("/", controller.getStudents);
+router.get("/:id", controller.getStudentsById);
 
 module.exports = router;
