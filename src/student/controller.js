b33a0763ef9ac49124777080ab9f07b63eea3174
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 20:44:38 +0530

    update

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,5 +1,10 @@
+const pool = require("../../db");
+
 const getStudents = (req, res) => {
-  console.log("getting students");
+  pool.query("SELECT * FROM students", (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
 };
 
 module.exports = {
