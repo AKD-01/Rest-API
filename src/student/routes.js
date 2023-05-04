4adefd80c033c7f3ea89b7a9098191cef984a6ec
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 22:33:32 +0530

    update

--- src/student/routes.js
+++ src/student/routes.js
@@ -4,6 +4,7 @@ const controller = require("./controller");
 const router = Router();
 
 router.get("/", controller.getStudents);
+router.post("/", controller.addStudent);
 router.get("/:id", controller.getStudentsById);
 
 module.exports = router;
