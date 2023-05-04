f8d0ef575baf88322e30e862e568a70113667276
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 23:11:54 +0530

    UPDATE

--- src/student/controller.js
+++ src/student/controller.js
@@ -17,13 +17,25 @@ const getStudentsById = (req, res) => {
 };
 
 const addStudent = (req, res) => {
-    const {name, email, age, dob} = req.body;
-    //check if email exists
-    pool.query(queries.checkEmailExists, [email], (error, results) => {
-        if(results.rows.length) {
-            res.send("Email already exists.");
+  const { name, email, age, dob } = req.body;
+  //check if email exists
+  pool.query(queries.checkEmailExists, [email], (error, results) => {
+    if (results.rows.length) {
+      res.send("Email already exists.");
+    }
+
+    //add student to db
+    else {
+      pool.query(
+        queries.addStudent,
+        [name, email, age, dob],
+        (error, results) => {
+          if (error) throw error;
+          res.status(201).send("Student Created Successfully!");
         }
-    });
+      );
+    }
+  });
 };
 
 module.exports = {
