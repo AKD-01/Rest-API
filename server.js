a3c21b1ae33d218da22bb52002065bb1b14c0345
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 20:44:08 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const studentRoutes = require("./src/student/routes");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
