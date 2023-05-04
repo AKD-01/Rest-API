a641e54f47620b6f366d712da4d196b3c4c394d8
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 20:44:20 +0530

    update

--- src/student/routes.js
+++ src/student/routes.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
+const controller = require('./controller');
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("using api route");
-});
+router.get("/", controller.getStudents);
 
 module.exports = router;
