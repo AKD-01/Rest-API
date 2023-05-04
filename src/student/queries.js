2d3e071688595a80fb91047998c3f02192195fd2
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-05-04 23:36:56 +0530

    update

--- src/student/queries.js
+++ src/student/queries.js
@@ -2,10 +2,12 @@ const getStudents = "SELECT * FROM students";
 const getStudentsById = "SELECT * FROM students WHERE id = $1"; //$1 is a parameter not a constant
 const checkEmailExists = "SELECT * FROM students WHERE email = $1";
 const addStudent = "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
+const removeStudent = "DELETE FROM students WHERE id = $1";
 
 module.exports = {
   getStudents,
   getStudentsById,
   checkEmailExists,
   addStudent,
+  removeStudent,
 };
